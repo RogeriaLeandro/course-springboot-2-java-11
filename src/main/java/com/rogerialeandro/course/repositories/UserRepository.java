a256db9910de1f805f43b09e0deef6cff144b6d8
Rogeria Leandro <rogeriaaraujoleandro@gmail.com>
2021-04-10 22:55:23 -0300

    Service layer, component registration

--- src/main/java/com/rogerialeandro/course/repositories/UserRepository.java
+++ src/main/java/com/rogerialeandro/course/repositories/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.rogerialeandro.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	//por herdar JpaRepository, ela já é uma annotation Repository
 }
